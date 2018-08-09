7a45579b7a28282e92175a5bbbde567178ca463b
Alexandria Hart <alexandriahart@moondog.local>
2018-08-09 12:28:51 -0600

    basic redux setup

---
+++ src/redux/reducers/user.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+const GET_USER = 'GET_USER'
+//promise middleware:
+const GET_USER_FULFILLED = 'GET_USER_FULFILLED'
+
+
+
+let initialState = {
+  data: null
+}
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_USER_FULFILLED:
+      return {...state, data: action.payload.data}
+    default:
+      return state
+  }
+}
+
+export function getUser(){
+  return {
+    type: GET_USER,
+    payload: axios.get('/api/currentUser')
+  }
+}
